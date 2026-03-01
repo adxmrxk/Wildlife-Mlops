4c95c665ca2bf3a63a3d32e1432d92579ce02a61
Adam <adamrak239@gmail.com>
2026-02-28 21:58:22 -0500

    Add Docker containerization for all services

--- ml-pipeline/app.py
+++ ml-pipeline/app.py
@@ -53,7 +53,7 @@ app = FastAPI(
 # Enable CORS for backend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    allow_origins=["http://localhost:8080", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
